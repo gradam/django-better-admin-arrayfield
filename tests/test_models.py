e5e6eb9b213b72a7df45b66d12af51da8f3523d7
Jakub Semik <kuba.semik@gmail.com>
2019-02-20 08:58:49 +0100

    Setup tests with pytest, add black and flake8 in pre-commit, change python/django versions in tox.ini

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,18 +8,6 @@ test_django-better-admin-arrayfield
 Tests for `django-better-admin-arrayfield` models module.
 """
 
-from django.test import TestCase
 
-from django_better_admin_arrayfield import models
-
-
-class TestDjango_better_admin_arrayfield(TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
+def test_base():
+    assert 2 == 2
