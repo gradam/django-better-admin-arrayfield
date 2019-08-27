e50de993320dfc51844f344c6356d612b2dcd5dd
dtovmasyan <davit.tovmasyan@dataart.com>
2019-08-27 14:32:51 +0400

    Fix `Add another` behavior when there are no existing items
    Issue #11

--- django_better_admin_arrayfield/static/js/django_better_admin_arrayfield.js
+++ django_better_admin_arrayfield/static/js/django_better_admin_arrayfield.js
@@ -10,18 +10,21 @@ window.addEventListener('load', function () {
 
     document.querySelectorAll('.dynamic-array-widget').forEach(function (widgetElement) {
 
+        var inititalElement = widgetElement.querySelector('.array-item');
+        var elementTemplate = inititalElement.cloneNode(true);
+        var parentElement = inititalElement.parentElement;
+
         addRemoveEventListener(widgetElement);
 
         widgetElement.querySelector('.add-array-item').addEventListener('click', function () {
-            var first = widgetElement.querySelector('.array-item');
-            var newElement = first.cloneNode(true);
+            var newElement = elementTemplate.cloneNode(true);
             var id_parts = newElement.querySelector('input').getAttribute('id').split('_');
             var id = id_parts.slice(0, -1).join('_') + '_' + String(parseInt(id_parts.slice(-1)[0]) + 1);
             newElement.querySelector('input').setAttribute('id', id);
             newElement.querySelector('input').value = '';
 
             addRemoveEventListener(newElement);
-            first.parentElement.appendChild(newElement);
+            parentElement.appendChild(newElement);
         });
 
     });
