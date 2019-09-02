9b203331572c43cec0beb80ce9830251e60137dc
Sergei Maertens <sergei-maertens@users.noreply.github.com>
2019-09-02 14:59:00 +0200

    :bug: Fixes #13 -- don't call static at import time (#14)
    Calling `static` at import time breaks when you use the manifest
    storage because of a missing entry (which happens when you're running
    collectstatic).

--- django_better_admin_arrayfield/admin/mixins.py
+++ django_better_admin_arrayfield/admin/mixins.py
@@ -1,7 +1,4 @@
-from django.templatetags.static import static
-
-
 class DynamicArrayMixin:
     class Media:
-        js = (static("js/min/django_better_admin_arrayfield.min.js"),)
-        css = {"all": (static("css/min/django_better_admin_arrayfield.min.css"),)}
+        js = ("js/min/django_better_admin_arrayfield.min.js",)
+        css = {"all": ("css/min/django_better_admin_arrayfield.min.css",)}
