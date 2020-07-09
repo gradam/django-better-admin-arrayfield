749bf2189290636aee8327201758f10b8e99a6f1
Jakub Semik <kuba.semik@gmail.com>
2020-07-09 10:56:43 +0200

    add tests
    Signed-off-by: Jakub Semik <kuba.semik@gmail.com>

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -21,3 +21,21 @@ def test_field_required():
         field.clean(data)
     data = ["12", "13"]
     field.clean(data)
+
+
+def test_default():
+    default = ["1"]
+    field = DynamicArrayField(CharField(max_length=10), required=True, default=default)
+    data = []
+    cleaned_data = field.clean(data)
+    assert cleaned_data == default
+
+
+def test_callable_default():
+    def default():
+        return ["1", "2"]
+
+    field = DynamicArrayField(CharField(max_length=10), required=True, default=default)
+    data = []
+    cleaned_data = field.clean(data)
+    assert cleaned_data == default()
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,15 +1,16 @@
 from typing import Type
 
-from django.forms import ModelForm
+from django.forms import ModelForm, IntegerField
 
 import pytest
 
+from django_better_admin_arrayfield.forms.fields import DynamicArrayField
 from tests.testapp.models import DefaultValueNullableModel, DefaultValueRequiredModel, NullableNoDefaultModel
 
 
-def form_factory(model) -> Type[ModelForm]:
+def form_factory(model, **attrs) -> Type[ModelForm]:
     meta = type("Meta", (), {"model": model, "fields": ["array"]})
-    return type("SampleForm", (ModelForm,), {"Meta": meta})
+    return type("SampleForm", (ModelForm,), {"Meta": meta, **attrs})
 
 
 class TestDefaultValue:
@@ -30,5 +31,30 @@ class TestDefaultValue:
             data["array"] = init_value
 
         form = form_factory(model_class)(data=data)
+        assert form.is_valid()
+        assert form.cleaned_data["array"] == expected_value
+
+
+class TestFormDefaultField:
+    @pytest.mark.parametrize(
+        "model_class,init_value,expected_value",
+        (
+            (DefaultValueNullableModel, None, [1]),
+            (NullableNoDefaultModel, None, [1]),
+            (DefaultValueRequiredModel, None, [1]),
+            (DefaultValueNullableModel, [2], [2]),
+            (NullableNoDefaultModel, [2], [2]),
+            (DefaultValueRequiredModel, [2], [2]),
+        ),
+    )
+    def test_default_different_values(self, model_class, init_value, expected_value):
+        default = [1]
+        data = {}
+        if init_value is not None:
+            data["array"] = init_value
+
+        field = DynamicArrayField(IntegerField(), required=True, default=default)
+
+        form = form_factory(model_class, array=field)(data=data)
         assert form.is_valid()
         assert form.cleaned_data["array"] == expected_value
